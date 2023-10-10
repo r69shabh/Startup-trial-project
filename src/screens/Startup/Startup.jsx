20ef1c135aff27ef33e4b69041a583bd0bc97907
r69shabh <143521130+r69shabh@users.noreply.github.com>
2023-10-11 05:00:44 +0530

    Add files via upload

---
+++ src/screens/Startup/Startup.jsx
@@ -0,0 +1,268 @@
+import React from "react";
+import "./style.css";
+
+export const Startup = () => {
+  return (
+    <div className="startup">
+      <div className="div">
+        <div className="overlap">
+          <div className="BG" />
+          <div className="service">
+            <p className="smart-jackpots-that">Grow your startup with our Service</p>
+            <p className="text-wrapper">
+              Build an incredible workplace and grow your business with Gusto’s all-in-one platform with amazing
+              contents.
+            </p>
+            <div className="service-2">
+              <div className="element">
+                <div className="fast-performance">Marketing &amp; advertising</div>
+                <p className="get-your-blood-tests">
+                  Get your info tests delivered at home collect a sample from the your pogress tests.
+                </p>
+                <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-58-1@2x.png" />
+              </div>
+              <div className="element-2">
+                <div className="fast-performance">Ultimate development</div>
+                <p className="get-your-blood-tests">
+                  Get your info tests delivered at home collect a sample from the your pogress tests.
+                </p>
+                <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-62-1@2x.png" />
+              </div>
+              <div className="element-3">
+                <div className="fast-performance">Business Enterprise</div>
+                <p className="get-your-blood-tests">
+                  Get your info tests delivered at home collect a sample from the your pogress tests.
+                </p>
+                <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-64@2x.png" />
+              </div>
+            </div>
+          </div>
+          <div className="customer">
+            <div className="overlap-group">
+              <div className="dividor" />
+              <div className="active" />
+            </div>
+            <div className="quote">
+              <div className="overlap-group-2">
+                <p className="p">
+                  “Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and
+                  is downright impressive when clients see a Qwilr Page we have made for them.”
+                </p>
+                <img className="img" alt="Quote" src="https://c.animaapp.com/9PagK4Pm/img/quote@2x.png" />
+              </div>
+              <p className="core-features">By Denny Albuz, Adviser of Paypal</p>
+              <div className="dash" />
+            </div>
+            <div className="logos">
+              <img className="image-2" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-11@2x.png" />
+              <img className="image-3" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-12@2x.png" />
+              <img className="image-4" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-13@2x.png" />
+              <img className="image-5" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-14@2x.png" />
+              <img className="image-6" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-15@2x.png" />
+            </div>
+          </div>
+        </div>
+        <header className="header">
+          <div className="text">
+            <p className="text-wrapper-2">Creativity starts with viewing the whole world differently</p>
+            <p className="lorem-ipsum-dolor">A service agency with customer satisfaction</p>
+          </div>
+          <div className="overlap-2">
+            <button className="button">
+              <div className="lorem-ipsum-dolor-wrapper">
+                <div className="lorem-ipsum-dolor-2">Get Started</div>
+              </div>
+            </button>
+            <img
+              className="illustration"
+              alt="Illustration"
+              src="https://c.animaapp.com/9PagK4Pm/img/illustration.png"
+            />
+          </div>
+          <div className="menu">
+            <div className="home-adversite">Login</div>
+            <div className="overlap-3">
+              <div className="home-adversite-2">Get Started</div>
+              <img className="path" alt="Path" src="https://c.animaapp.com/9PagK4Pm/img/path-49.svg" />
+            </div>
+            <p className="home-adversite-3">
+              <span className="span">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Adversite</span>
+              <span className="text-wrapper-3">&nbsp;</span>
+              <span className="text-wrapper-4"></span>
+              <span className="text-wrapper-3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+              <span className="span">Supports</span>
+              <span className="text-wrapper-3">&nbsp;</span>
+              <span className="text-wrapper-4"></span>
+              <span className="text-wrapper-3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+              <span className="span">About</span>
+            </p>
+            <div className="logo">
+              <div className="petro">StartupLanding</div>
+              <img className="s" alt="S" src="https://c.animaapp.com/9PagK4Pm/img/s-1.svg" />
+            </div>
+          </div>
+        </header>
+        <div className="ultimate-features">
+          <div className="text-2">
+            <p className="smart-jackpots-that-2">Other sevices you must appreciate</p>
+            <div className="flexcontainer">
+              <p className="span-wrapper">
+                <span className="text-wrapper-5">
+                  Focus only on the meaning, we take care of the design. As soon as the <br />
+                </span>
+              </p>
+              <p className="span-wrapper">
+                <span className="text-wrapper-5">meeting end you can export in one click into your preferred.</span>
+              </p>
+            </div>
+          </div>
+          <div className="features">
+            <div className="features-2">
+              <div className="fast-performance">3D modeling &amp; art</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-65@2x.png" />
+            </div>
+            <div className="features-3">
+              <div className="fast-performance">Digital promotion</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-66@2x.png" />
+            </div>
+            <div className="features-4">
+              <div className="fast-performance">Business Enterprise</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-67@2x.png" />
+            </div>
+            <div className="features-5">
+              <div className="fast-performance">Marketing &amp; advertising</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-58@2x.png" />
+            </div>
+            <div className="features-6">
+              <div className="fast-performance">Ultimate development</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-62@2x.png" />
+            </div>
+            <div className="features-7">
+              <div className="fast-performance">Online support</div>
+              <p className="get-your-blood-tests">
+                Get your info tests delivered at home collect a sample from the your pogress tests.
+              </p>
+              <div className="learn-more">
+                <div className="lorem-ipsum-dolor-3">Learn more</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/9PagK4Pm/img/vector-5.svg" />
+              </div>
+              <img className="image" alt="Image" src="https://c.animaapp.com/9PagK4Pm/img/image-68@2x.png" />
+            </div>
+          </div>
+        </div>
+        <div className="subscirbe">
+          <div className="subscribe">
+            <div className="overlap-4">
+              <p className="get-your-blood-tests-2">
+                We have more than thousand of creative entrepreneurs and stat joining our business
+              </p>
+              <p className="text-wrapper-6">Like our service? Subscribe us</p>
+              <div className="field">
+                <div className="checkbox">
+                  <div className="BG-2" />
+                  <p className="get-your-blood-tests-3">Don’t provide any promotional message.</p>
+                </div>
+                <div className="feild">
+                  <div className="div-wrapper">
+                    <div className="lorem-ipsum-dolor-4">Enter Email address</div>
+                  </div>
+                </div>
+                <div className="overlap-wrapper">
+                  <div className="overlap-5">
+                    <div className="lorem-ipsum-dolor-5">Subscribe</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footers">
+          <div className="text-wrapper-7">About Us</div>
+          <div className="group">
+            <p className="terms-of-use-privacy">Terms of use&nbsp;&nbsp; |&nbsp;&nbsp; Privacy</p>
+            <p className="text-wrapper-8">Copyright by 2019 YourName, Inc</p>
+            <div className="logo">
+              <div className="petro-2">StartupLanding</div>
+              <img className="s" alt="S" src="https://c.animaapp.com/9PagK4Pm/img/s-1-1.svg" />
+            </div>
+          </div>
+          <div className="support-center">
+            Support Center
+            <br />
+            Customer Support
+            <br />
+            About Us
+            <br />
+            Copyright
+          </div>
+          <div className="text-wrapper-9">Our Information</div>
+          <p className="return-policy">
+            Return Policy <br />
+            Privacy Policy
+            <br />
+            Terms &amp; Conditions
+            <br />
+            Site Map
+          </p>
+          <div className="text-wrapper-10">My Account</div>
+          <p className="press-inquiries">
+            Press inquiries
+            <br />
+            Social media <br />
+            directories
+            <br />
+            Images &amp; B-roll
+          </p>
+          <div className="group-2">
+            <div className="text-wrapper-11">Connect</div>
+            <div className="facebook-twitter">
+              Facebook
+              <br />
+              Twitter
+              <br />
+              Github
+              <br />
+              Dribbble
+            </div>
+            <img className="social" alt="Social" src="https://c.animaapp.com/9PagK4Pm/img/social@2x.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
